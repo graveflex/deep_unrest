0f8d31907c222ed0b87c346d4e0f7ac4d8f66313
Lynn Hurley <lynn.dylan.hurley@gmail.com>
2017-05-18 00:15:10 -0500

    codeclimate config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,14 @@
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
+# start coverage
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path('../../test/dummy/config/environment.rb', __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require 'rails/test_help'
-require 'simplecov'
-
-# start coverage
-SimpleCov.start
 
 DatabaseCleaner.strategy = :transaction
 
