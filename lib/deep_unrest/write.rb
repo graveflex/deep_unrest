0d0c59fb7d610d1a1e1be8ea7e8143a55634eb1b
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2020-01-23 11:08:25 -0600

    [bugfix] incorrect logic in change list mapping

--- lib/deep_unrest/write.rb
+++ lib/deep_unrest/write.rb
@@ -164,7 +164,9 @@ module DeepUnrest
       changed.select { |c| c[:query_uuid] }
              .each_with_object({}) do |c, memo|
                mapping = mappings.find { |m| m.dig(:query, :uuid) == c[:query_uuid] }
-               next unless mapping || mapping[:addr].blank?
+               next unless mapping
+               next if mapping[:addr].blank?
+
                mapping[:query][:fields] = c[:attributes].keys
                mapping[:record] = c[:klass].new(id: c[:id])
                mapping[:record].assign_attributes(c[:attributes])
