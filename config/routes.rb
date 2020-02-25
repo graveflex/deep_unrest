38b12aa9ea4dba7aa8b544f125a6bb7e9b61acde
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2020-02-25 16:03:54 -0600

    [feat] allow read route access from patch method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DeepUnrest::Engine.routes.draw do
   patch 'update', to: 'application#update'
   get 'read', to: 'application#read'
+  patch 'read', to: 'application#read'
   patch 'write', to: 'application#write'
 end
